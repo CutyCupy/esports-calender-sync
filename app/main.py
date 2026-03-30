a2e717c20f3db4aedb3249e7172b3aad6ab404f2
Alexander Ciupka <alex.ciupka@gmx.de>
2026-03-30 21:07:55 +0200

    #2 & #3: implemented single url runs and ability to start single runs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import json
 import time
-from datetime import datetime, timedelta
 
 
 from context import Context
@@ -8,29 +7,30 @@ from google_calendar import add_match_to_google_calendar
 from casting_calendar import add_match_to_casting_calendar, commit_casting_calendar
 from config import Config # type: ignore
 from logger import cleanup_logs, setup_run_logger
-from parser import CALENDAR_SERVICE, GERMAN_WEEKDAYS, SHEETS_SERVICE, TZ, parse_url
-
-from match import CastInfo, Match
+from parser import parse_url
 
 
-    
-def main():
+def run(team = None) -> bool:
     logger, log_file = setup_run_logger()
     
     ctx = Context(
         logger=logger, log_file=log_file, config=Config.load()
     )
     
+    teams = [team] if team else ctx.config.teams
+    result = True
+    
     cleanup_logs(days=7)
     start = time.time()
     
     logger.info("Start der Verarbeitung")
-    for url in ctx.config.teams:
+    for url in teams:
         logger.info("Start der Verarbeitung von '%s'", url)
         try:
             matches = parse_url(ctx, url.strip())
         except Exception:
             logger.exception("Failed processing URL: %s", url, stack_info=True)
+            result = False
             continue
 
         for match in matches:
@@ -49,11 +49,13 @@ def main():
                     match.id,
                     json.dumps(match, indent=2, default=str)
                 )
+                result = False
         
     commit_casting_calendar(ctx)
     logger.info("Verarbeitung nach %.2fs abgeschlossen.", time.time() - start)
     logger.info("Logfile: %s", log_file)
-
+    
+    return result
 
 if __name__ == "__main__":
-    main()
+    run()
